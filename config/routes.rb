e2c5e8640fd74264326cfef7b9c2063423b6f329
Italo Assai <italoassai2012@gmail.com>
2023-10-25 10:39:47 -0300

    scheduled tweets commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,39 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  get "about-us", to: "about#index", as: :about
+
+  get "password", to: "passwords#edit", as: :edit_password
+
+  patch "password", to: "passwords#update"
+
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+
+  post "sign_in", to: "sessions#create"
+
+ delete "logout", to: "sessions#destroy"
+
+ get "password/reset", to: "password_resets#new"
+
+ post "password/reset", to: "password_resets#create"
+
+ get "password/reset/edit", to: "password_resets#edit"
+
+ patch "password/reset/edit", to: "password_resets#update"
+
+
+
+
+ root to: "main#index"
+
+end
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   # GET /about
-  get "about-us", to: "about#index", as: :about
-
-
- root to: "main#index"
   # Defines the root path route ("/")
   # root "posts#index"
-end
